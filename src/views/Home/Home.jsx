ccd49802f55365d3c64b55275eabfba3a60790ab
Riccardo Frighi <r.frighi@campus.unimib.it>
2025-01-20 23:20:13 +0100

    20/01/2025: - Warning fixed

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,7 +1,7 @@
 import {Input} from "@heroui/react";
 import TCGBannerGrid from "../../components/TCGBannerGrid/TCGBannerGrid.jsx";
 import {MagnifyingGlassIcon} from "@heroicons/react/16/solid/index.js";
-import {useRef, useState} from "react";
+import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 
 const Home = () => {
