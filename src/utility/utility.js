4166bf0819f2955fc455731f4d7bf69738c97581
Riccardo Frighi <r.frighi@campus.unimib.it>
2025-02-01 18:32:11 +0100

    Maybe it's working

--- src/utility/utility.js
+++ src/utility/utility.js
@@ -3,8 +3,6 @@ import PokemonLogo from "../assets/images/pokemon-logo.png";
 
 export const reverseDate = (str) => str.split('/').reverse().join('/');
 
-export const getPokemonImage = (id) => `https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/${zeroPad(id, 3)}.png`;
-
 export const pokemonDefaultImage = (onErrorEvent) =>  onErrorEvent.target.src = PokemonBack;
 
 export const pokemonLogo = PokemonLogo;
