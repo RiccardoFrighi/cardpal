f393bb9960accba09db08bc9fa6ff26cb95f81e5
Riccardo Frighi <r.frighi@campus.unimib.it>
2025-02-01 18:22:18 +0100

    I'm losing hope

--- src/components/TCGBannerGrid/TCGBannerGrid.jsx
+++ src/components/TCGBannerGrid/TCGBannerGrid.jsx
@@ -1,15 +1,21 @@
-import TCGListData from "../../assets/data/tcgs.json";
 import TCGBannerGridCard from "./TCGBannerGridCard.jsx";
+import pokemon from "../../assets/images/pokemon-banner.png";
+import lorcana from "../../assets/images/lorcana-banner.png";
+import onepiece from "../../assets/images/onepiece-banner.png";
+import yugioh from "../../assets/images/yugioh-banner.png";
+import magic from "../../assets/images/magic-banner.png";
+import starwars from "../../assets/images/starwars-banner.png";
 
 const TCGBannerGrid = () => {
 
     return (
         <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-2 px-4 xl:px-0">
-            {TCGListData.map((item) => {
-                return (
-                    <TCGBannerGridCard key={item.url} text={item.text} image={item.image} active={item.active} url={item.url} />
-                )
-            })}
+            <TCGBannerGridCard key={"pokemon"} text={"Pokémon"} image={pokemon} active={true} url={"tcg/pokemon"} />
+            <TCGBannerGridCard key={"lorcana"} text={"Lorcana"} image={lorcana} active={false} url={"tcg/lorcana"} />
+            <TCGBannerGridCard key={"onepiece"} text={"One Piece"} image={onepiece} active={false} url={"tcg/onepiece"} />
+            <TCGBannerGridCard key={"magic"} text={"Magic"} image={magic} active={false} url={"tcg/magic"} />
+            <TCGBannerGridCard key={"yugioh"} text={"Yu-gi-oh!"} image={yugioh} active={false} url={"tcg/yugioh"} />
+            <TCGBannerGridCard key={"starwars"} text={"Star Wars Unlimited"} image={starwars} active={false} url={"tcg/starwars"} />
         </div>
     )
 }
